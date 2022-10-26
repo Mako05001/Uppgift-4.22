b3d8143c7911188a279fd9a44730cd0e6f5d415f
MAKSY <MAKSY@DESKTOP-6S4ESUP>
2022-10-26 20:22:57 +0200

    Add project files.

---
+++ Uppgift 4.22/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+for (int x = 10; x < 31; x++)
+{
+    Console.Write(" " + x);
+}
+Console.WriteLine();
+for (int x = 200; x >= 180; x--)
+{
+    Console.Write(" " +x);
+}
+Console.WriteLine();
+for (int x = 1000; x <= 1400; x+= 50)
+{
+    Console.Write(" " + x);
+}
+Console.WriteLine();
